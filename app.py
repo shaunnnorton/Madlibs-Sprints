d4b300f89d0d56f8c625056cff3e0cb689bcb339
shaunnorton <shaunnorton87@gmail.com>
2020-11-28 21:45:50 -0800

    Changed some parts of existing files to be more efficient and created routes and templates for image paths

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/madlibsdb"
 mongo = PyMongo(app)
-'''mongo.db.madlibs.insert_one({
+'''mongo.db.madlibs_image.insert_one({
     'theme':'animals',
-    'length':'short',
+    'length':'long',
     'answers':['adjective','animal','animal-2','noun','adjective-2','noun-2','adjective-3','adjective-4','noun-3'],
-    'text':"Camping is {{0}}. You might see {{1}} and {{2}}. The food which is cooked over the {{3}} is very {{4}}.                 The best part of camping is using the {{5}} which is very {{6}}. At night, the tent is extreamly {{7}}. It's always best to take a {{8}} camping with you."
+    'text':"Camping is {0}. You might see {1} and {2}. The food which is cooked over the {3} is very {4}.                 The best part of camping is using the {5} which is very {6}. At night, the tent is extreamly {{7}}. It's always best to take a {8} camping with you."
 })'''
 @app.route('/')
 def homepage():
@@ -19,7 +19,7 @@ def classicCreate():
     theme = request.args.get('theme')
     length = request.args.get('length')
     if theme and length:
-        madlib_results = mongo.db.madlibs.find_one({'theme':theme,'length':length})
+        madlib_results = mongo.db.madlibs_classic.find_one({'theme':theme,'length':length})
     else:
         madlib_results = {'answers':[]}
     context = {
@@ -34,7 +34,7 @@ def finishedClassic():
     results = []
     for result in request.args:
         results.append(request.args.get(result))
-    madlib_text_raw = mongo.db.madlibs.find_one({'theme':request.args.get('theme'),'length':request.args.get('length')})['text']
+    madlib_text_raw = mongo.db.madlibs_classic.find_one({'theme':request.args.get('theme'),'length':request.args.get('length')})['text']
     madlib_text = ''
     current_result = 0 
     for result in results:
@@ -47,9 +47,46 @@ def finishedClassic():
     }
     return render_template('classic-completed.html',**context)
 
+@app.route('/Create/Image')
+def imageCreate():
+    theme = request.args.get('theme')
+    length = request.args.get('length')
+    if theme and length:
+        madlib_results = mongo.db.madlibs_image.find_one({'theme':theme,'length':length})
+    else:
+        madlib_results = {'answers':[]}
+    context = {
+        'answers': madlib_results['answers'],
+        'theme':theme,
+        'length':length
+    }  
+    return render_template('image-create.html', **context)
 
 
-
+@app.route('/Image')
+def finishedImage():
+    results = []
+    for result in request.args:
+        results.append(request.args.get(result))
+    madlib_text_raw = mongo.db.madlibs_image.find_one({'theme':request.args.get('theme'),'length':request.args.get('length')})['text']
+    madlib_text_split = madlib_text_raw.split()
+    print(madlib_text_split)
+    current_result = 0 
+    for result in results:
+        if current_result >= len(results)-2:
+            break
+        index_of_result = madlib_text_split.index(f'{{{current_result}}}')
+        madlib_text_split[index_of_result] = result
+        
+        
+        current_result+=1
+    print(madlib_text_split)
+    context = {
+        
+        'madlib':madlib_text_split,
+        'results':results
+    }
+    return render_template('image-completed.html',**context)
 
 
 
